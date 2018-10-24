aff985838b3fdaae75fc9414df1d8d905ab3cc32
钟杜生 <zhongdusheng@xunlei.com>
2018-10-24 18:40:37 +0800

    add route

--- router.js
+++ router.js
@@ -30,4 +30,32 @@ router.post('/ceph/createpool', (ctx, next) => {
     }
 });
 
+router.post('/api/kubernetes/storageclass', (ctx, next) => {
+    ctx.body = [
+        {
+            "id": 1,
+            "scnmae": "test-sc",
+            "scfstype": "ext4",
+            "createtime": "2018-01-03",
+            "cephcluster": "ceph-test",
+            "k8scluster": "tw06m1",
+            "pool": "kubernetes",
+            "group": "monitor"
+        },
+        {
+            "id": 2,
+            "scnmae": "account-sc",
+            "scfstype": "ext4",
+            "createtime": "2018-01-03",
+            "k8scluster": "tw06m1",
+            "cephcluster": "ceph-test",
+            "pool": "kubernetes",
+            "group": "account"
+        },
+    ]
+});
+
+
+
+
 module.exports = router
